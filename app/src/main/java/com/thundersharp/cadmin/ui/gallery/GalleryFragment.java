5464c059e99804041a3e18bf80b2b4746add8b0a
hrishikeshprateek <prateekhrishikesh@gmail.com>
2020-07-14 15:00:02 +0530

    updated index

--- app/src/main/java/com/thundersharp/cadmin/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/thundersharp/cadmin/ui/gallery/GalleryFragment.java
@@ -16,20 +16,11 @@ import com.thundersharp.cadmin.R;
 
 public class GalleryFragment extends Fragment {
 
-    private GalleryViewModel galleryViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        galleryViewModel =
-                ViewModelProviders.of(this).get(GalleryViewModel.class);
         View root = inflater.inflate(R.layout.fragment_gallery, container, false);
-        final TextView textView = root.findViewById(R.id.text_gallery);
-        galleryViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+
         return root;
     }
 }
--- app/src/main/java/com/thundersharp/cadmin/ui/slideshow/SlideshowFragment.java
+++ app/src/main/java/com/thundersharp/cadmin/ui/slideshow/SlideshowFragment.java
@@ -16,20 +16,11 @@ import com.thundersharp.cadmin.R;
 
 public class SlideshowFragment extends Fragment {
 
-    private SlideshowViewModel slideshowViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        slideshowViewModel =
-                ViewModelProviders.of(this).get(SlideshowViewModel.class);
         View root = inflater.inflate(R.layout.fragment_slideshow, container, false);
-        final TextView textView = root.findViewById(R.id.text_slideshow);
-        slideshowViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+
         return root;
     }
 }
