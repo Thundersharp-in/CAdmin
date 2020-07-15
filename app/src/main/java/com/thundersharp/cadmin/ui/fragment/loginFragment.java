b2aa12c9e285f8e861e5e33329640bebc042e96e
prajjwalkumar17 <Dablu01@tcs>
2020-07-15 15:31:30 +0530

    layout

---
+++ app/src/main/java/com/thundersharp/cadmin/ui/fragment/loginFragment.java
@@ -0,0 +1,26 @@
+package com.thundersharp.cadmin.ui.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.thundersharp.cadmin.R;
+
+import java.util.zip.Inflater;
+
+
+public class loginFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_login, container, false);
+        return view;
+    }
+}
