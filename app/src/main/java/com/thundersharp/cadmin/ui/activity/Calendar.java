af18a14135d39a39e90400a13f0f04ff49ae82a2
hrishikeshprateek <prateekhrishikesh@gmail.com>
2020-07-16 23:20:43 +0530

    multiple sections updated

--- app/src/main/java/com/thundersharp/cadmin/ui/activity/Calendar.java
+++ app/src/main/java/com/thundersharp/cadmin/ui/activity/Calendar.java
@@ -1,23 +1,25 @@
 package com.thundersharp.cadmin.ui.activity;
 
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
 import com.thundersharp.cadmin.R;
 
 
+
 public class Calendar extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_calendar);
+
+
     }
 }
