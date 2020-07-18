0352c265a1e2019defa12b8193606793c26965ed
AmritaSingh09 <AmritaSingh09@users.noreply.github.com>
2020-07-18 23:26:57 +0530

    organisation upload to database

--- app/src/main/java/com/thundersharp/cadmin/ui/fragment/Organisation.java
+++ app/src/main/java/com/thundersharp/cadmin/ui/fragment/Organisation.java
@@ -1,16 +1,17 @@
 package com.thundersharp.cadmin.ui.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
-import com.thundersharp.cadmin.ui.activity.MainActivity;
 import com.thundersharp.cadmin.R;
+import com.thundersharp.cadmin.ui.activity.AddOrganisation;
+import com.thundersharp.cadmin.ui.activity.MainActivity;
 
 import static com.thundersharp.cadmin.ui.activity.MainActivity.floatingActionButton;
 
@@ -20,12 +21,14 @@ public class Organisation extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_gallery, container, false);
+
         MainActivity.container.setBackground(null);
-        floatingActionButton.setImageDrawable(getResources().getDrawable(R.drawable.ic_baseline_chat_bubble_outline_24,getActivity().getTheme()));
+        floatingActionButton.setImageDrawable(getResources().getDrawable(R.drawable.ic_add_black_24dp,getActivity().getTheme()));
         floatingActionButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getContext(),"Chat comming soon",Toast.LENGTH_SHORT).show();
+                Intent intent=new Intent(getContext(), AddOrganisation.class);
+                startActivity(intent);
             }
         });
 
---
+++ calendar/build/generated/source/buildConfig/debug/com/thundersharp/calendar/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.thundersharp.calendar;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.thundersharp.calendar";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "0.4.0";
+}
