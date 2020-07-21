081da2617e90b999bbf4d62c16a9f23e15b7dd95
hrishikeshprateek <prateekhrishikesh@gmail.com>
2020-07-21 18:37:12 +0530

    Updated CalendarCursor

--- app/src/main/java/com/thundersharp/cadmin/ui/activity/Login_reg.java
+++ app/src/main/java/com/thundersharp/cadmin/ui/activity/Login_reg.java
@@ -29,13 +29,13 @@ public class Login_reg extends AppCompatActivity {
 
     @Override
     public void onBackPressed() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(getApplicationContext());
+        AlertDialog.Builder builder = new AlertDialog.Builder(Login_reg.this);
         builder.setTitle("Sure to exit !!");
         builder.setMessage("Do you really want to exit the application !!");
         builder.setPositiveButton("EXIT", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
-                Login_reg.super.onBackPressed();
+                finish();
             }
         });
         builder.setNegativeButton("NO", new DialogInterface.OnClickListener() {
