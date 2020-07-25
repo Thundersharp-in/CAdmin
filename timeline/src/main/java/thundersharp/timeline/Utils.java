ba413def2eaae825be7a1e5fdad8baf7c4f27864
hrishikeshprateek <prateekhrishikesh@gmail.com>
2020-07-25 20:05:15 +0530

    Updated Register.java

---
+++ timeline/src/main/java/thundersharp/timeline/Utils.java
@@ -0,0 +1,17 @@
+package thundersharp.timeline;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.util.TypedValue;
+
+class Utils {
+
+    public static int dpToPx(float dp, Context context) {
+        return dpToPx(dp, context.getResources());
+    }
+
+    private static int dpToPx(float dp, Resources resources) {
+        float px = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, resources.getDisplayMetrics());
+        return (int) px;
+    }
+}
